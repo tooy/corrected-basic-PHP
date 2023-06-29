8dfa10dc4103de2cefff6684660830b1e3c3bf68
Flacher Romain <romain.flacher@gmail.com>
2023-06-29 15:03:48 +0200

    Étape 3.2 : ajout ficher 404.php

---
+++ pages/404.php
@@ -0,0 +1,10 @@
+<?php
+$metaTitle = '404 Not Found';
+$metaDescription = 'La page que vous recherchez n\'existe pas.';
+include('header.php');
+?>
+<main>
+    <h1>404 Not Found</h1>
+    <p>La page que vous recherchez n\'existe pas.</p>
+</main>
+<?php include('footer.php'); ?>
