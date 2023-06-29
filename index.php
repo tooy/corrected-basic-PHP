20443694f6389ac25a3b12415e75d697a4872ab9
Flacher Romain <romain.flacher@gmail.com>
2023-06-29 15:00:04 +0200

    Étape 3.2 : ajout page 404

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
-$page = $_GET['page'];
+$page = $_GET['page'] ?? '404';
 
-if($page=='page1'){
-    include('pages/page1.php');
-}elseif ($page == 'page2'){
-    include('pages/page2.php');
+if ($page == 'page1') {
+    include('pages/page1.phtml');
+} elseif ($page == 'page2') {
+    include('pages/page2.phtml');
+} else if ($page == '404') {
+    include "page/404.phtml";
 }
+
