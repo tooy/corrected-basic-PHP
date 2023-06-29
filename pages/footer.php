253347dc55ccc9dd793ff029cf9e0fe256d5e4eb
Flacher Romain <romain.flacher@gmail.com>
2023-06-29 15:07:36 +0200

    Etape 5 - Session

--- pages/footer.php
+++ pages/footer.php
@@ -1,5 +1,7 @@
 <footer>
     <!-- Votre code HTML pour le footer ici -->
+    <p>Vous avez visité ce site pour la première fois le : <?=$_SESSION['dateFirstVisit']?></p>
+    <p>Vous avez vu <?=$_SESSION['countViewPage']?> pages sur ce site.</p>
 </footer>
 </body>
 </html>
--- pages/header.php
+++ pages/header.php
@@ -1,3 +1,19 @@
+<?php
+session_start(); // Commence ou continue une session
+
+if (!isset($_SESSION['dateFirstVisit'])) {
+    // Si c'est la première visite, stocker la date actuelle
+    $_SESSION['dateFirstVisit'] = date("Y-m-d H:i:s");
+}
+
+if (!isset($_SESSION['countViewPage'])) {
+    // Initialise le compteur de pages vues s'il n'existe pas encore
+    $_SESSION['countViewPage'] = 0;
+} else {
+    // Si le compteur de pages vues existe déjà, l'incrémenter
+    $_SESSION['countViewPage']++;
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
